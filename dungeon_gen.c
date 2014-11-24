3156aeba50a9fa18dc90eea770be6c3f424d9a82
kit <kit@debian>
2014-11-25 02:31:23 +0900

    コメントを追加 まだ途中 	modified:   dungeon_gen.c 	modified:   dungeon_gen.h

--- dungeon_gen.c
+++ dungeon_gen.c
@@ -3,40 +3,82 @@
 
 int seed;
 
+static uchar nextDir(void);
+static void updateRange(uchar, uchar, uchar max[2], uchar min[2]);
+static uchar scanAround(uchar map[FIELD_SZ][FIELD_SZ], uchar, uchar);
+static uchar getBranchPoint(uchar, uchar);
+static void findBranchPoint(uchar map[FIELD_SZ][FIELD_SZ], uchar*, uchar*, uchar max[2], uchar min[2]);
+static uchar checkdig(uchar map[FIELD_SZ][FIELD_SZ], uchar, uchar, uchar);
+static void dig(uchar map[FIELD_SZ][FIELD_SZ], uchar*, uchar*, uchar, uint*, uchar max[2], uchar min[2]);
+
+// ランダムな方向を取得
 uchar nextDir(){
     return rand() % 4;
 }
 
-// 迷路を作成した範囲を更新する
+/*
+    迷路を作成した範囲を更新する
+
+    x   : 現在座標X
+    y   : 現在座標Y
+    max : 最大値 [x, y]
+    min : 最小値 [x, y]
+*/
 void updateRange(uchar x, uchar y, uchar max[2], uchar min[2]){
-    if(max[0] < x)  max[0] = x;
-    if(max[1] < y)  max[1] = y;
-    if(min[0] > x)  min[0] = x;
-    if(min[1] > y)  min[1] = y;
+    if(max[0] < x)  max[0] = x;     //　最大値を更新
+    if(max[1] < y)  max[1] = y;     //　最大値を更新
+    if(min[0] > x)  min[0] = x;     //　最小値を更新
+    if(min[1] > y)  min[1] = y;     //　最小値を更新
 }
 
-// 進行方向が残っているかを確認
+/*
+    進行方向が残っているかを確認
+
+    map : フィールド
+    x   : 現在座標X
+    y   : 現在座標Y
+
+    return  : TURE/FALSE 残っている/いない
+*/
 uchar scanAround(uchar map[FIELD_SZ][FIELD_SZ], uchar x, uchar y){
+    // 上下左右の4方向に対して，
+    // 2マス先が通路かつ，1マス先が最外壁でない
     if( (map[y][x+2] == ID_PASSAGE || FIELD_SZ-1 <= x+1)
      && (map[y+2][x] == ID_PASSAGE || FIELD_SZ-1 <= y+1)
      && (map[y][x-2] == ID_PASSAGE || x-1 <= 0)
      && (map[y-2][x] == ID_PASSAGE || y-1 <= 0)){
-        return 0;
+        return FALSE;
     }else{
-        return 1;
+        return TRUE;
     }
 }
 
+/*
+    分岐点を探す
+
+    max : 現在到達した最大座標
+    min : 現在到達した最小座標
+ */
 uchar getBranchPoint(uchar max, uchar min){
     uchar res;
 
-    res = (rand() + min) % max;
-    if(res % 2) res = res + 1 % 2;  // 奇数の時
+    do{
+        res = (rand() + min) % max;     // 範囲内でランダムな点を探す
+    }while(res != 0);
+    if(res % 2) res = res + 1 % 2;      // 奇数の時
 
     return res;
 }
 
-// 分岐点を探す
+/*
+    分岐点を探す
+
+    map : フィールド
+    x   : 分岐点のX座標
+    y   : 分岐点のY座標
+    max : 最大値
+    min : 最小値
+ */
 void findBranchPoint(uchar map[FIELD_SZ][FIELD_SZ], uchar *x, uchar *y, uchar max[2], uchar min[2]){
     do{
         *x = getBranchPoint(max[0], min[0]);
--- dungeon_gen.h
+++ dungeon_gen.h
@@ -7,12 +7,5 @@
 #include "user.h"
 #endif
 
-uchar nextDir();
-void updateRange(uchar x, uchar y, uchar max[2], uchar min[2]);
-uchar scanAround(uchar map[FIELD_SZ][FIELD_SZ], uchar x, uchar y);
-uchar getBranchPoint(uchar max, uchar min);
-void findBranchPoint(uchar map[FIELD_SZ][FIELD_SZ], uchar *x, uchar *y, uchar max[2], uchar min[2]);
-uchar checkdig(uchar map[FIELD_SZ][FIELD_SZ], uchar x, uchar y, uchar dir);
-void dig(uchar map[FIELD_SZ][FIELD_SZ], uchar* x, uchar* y, uchar dir, uint* plen, uchar max[2], uchar min[2]);
-void genDungeon(uchar map[FIELD_SZ][FIELD_SZ]);
+void genDungeon(uchar map[FIELD_SZ][FIELD_SZ]);     // ダンジョン生成
 
